60a69eee40ece1babdcf84b68887b5c352f5bfd6
jb-githubuser <140766607+jb-githubuser@users.noreply.github.com>
2024-07-17 15:55:40 -0400

    Delete alwaysDefect.java

--- alwaysDefect.java
+++
@@ -1,9 +0,0 @@
-public class alwaysDefect extends Player{
-    public alwaysDefect(String name){
-        super(name);
-    }
-    @Override
-    public boolean play(){
-        return false;
-    }
-}
